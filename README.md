e8e317d6b922ee42d12ecf946492426400f8c29b
unknown <Digenes Barradas>
2026-03-28 19:50:33 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# aprendendo-cypress# aprendendo-cypress
---
+++ cypress.config.js
@@ -0,0 +1,22 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  allowCypressEnv: false,
+
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+ browsers: [
+    {
+      name: 'brave',
+      channel: 'stable',
+      family: 'chromium',
+      displayName: 'Brave',
+      version: 'custom',
+      majorVersion: 120,
+      path: 'C:\\Program Files\\BraveSoftware\\Brave-Browser\\Application\\brave.exe'
+    }
+  ]
+})
