026dc2f2a6c480869edfc8d8aa8f2ae26b602be0
unknown <Digenes Barradas>
2026-04-08 19:59:25 -0300

    PageLoad Added!

--- cypress/pages/myInfoPage.js
+++ cypress/pages/myInfoPage.js
@@ -19,32 +19,45 @@ class MyInfoPage {
         return selectors;
     }
 
-    PersonalDetails() {
-            // cy.get(this.selectorsList().MyInfoButton).click()
-            cy.get(this.selectorsList().FirstNameField).clear().type('Diógenes', { delay: 100 })
-            cy.get(this.selectorsList().MiddleNameField).clear().type('Almeida Santos', { delay: 100 })
-            cy.get(this.selectorsList().LastNameField).clear().type('Barradas', { delay: 100 })
-            cy.get(this.selectorsList().genericField).eq(3).clear().type('003', {delay: 190})
-            cy.get(this.selectorsList().genericField).eq(4).clear().type('D200303', {delay: 150})
-            cy.get(this.selectorsList().genericField).eq(5).clear().type('DI0303', {delay: 170})
-            cy.get(this.selectorsList().genericField).eq(6).clear().type('003', {delay: 190})
-            cy.get(this.selectorsList().genericDateField).eq(0).clear().type('2033-03-02', {delay: 160})
+     FillPersonalDetails(firstName, middleName, lastName) {
+            cy.get(this.selectorsList().FirstNameField).clear().type(firstName, { delay: 100 })
+            cy.get(this.selectorsList().MiddleNameField).clear().type(middleName, { delay: 100 })
+            cy.get(this.selectorsList().LastNameField).clear().type(lastName, { delay: 100 })
+            
+    }
+
+    FillEmployeeDetails(employeeId, otherId, driversLicense, licenseExpiry, birthDate) {
+            cy.get(this.selectorsList().genericField).eq(3).clear().type(employeeId, {delay: 190})
+            cy.get(this.selectorsList().genericField).eq(4).clear().type(otherId, {delay: 150})
+            cy.get(this.selectorsList().genericField).eq(5).clear().type(driversLicense, {delay: 170})
+            cy.get(this.selectorsList().genericDateField).eq(0).clear().type(licenseExpiry, {delay: 160})
             cy.get(this.selectorsList().dateCloseButton).click()
             cy.get(this.selectorsList().infoDropdown).eq(0).click()
             cy.get(this.selectorsList().nationalityOption).click()
             cy.get(this.selectorsList().infoDropdown).eq(1).click()
             cy.get(this.selectorsList().maritalOption).click()
-            cy.get(this.selectorsList().genericDateField).eq(1).clear().type('2003-03-02', {delay: 160})
+            cy.get(this.selectorsList().genericDateField).eq(1).clear().type(birthDate, {delay: 160})
             cy.get(this.selectorsList().dateCloseButton).click()
-            cy.get(this.selectorsList().genderOption).eq(0).click()
+            cy.get(this.selectorsList().genderOption).eq(0).click() 
             cy.get(this.selectorsList().submitButton).eq(0).click()
+            cy.get('body').should('contain', 'Successfully Updated')         
+    }
+
+    FillCustomDetails() {
             cy.get(this.selectorsList().infoDropdown).eq(2).click()
             cy.get(this.selectorsList().bloodOption).click()
-            cy.get(this.selectorsList().testField).clear().type('The most common blood type in Brazil', {delay: 150})
+            cy.get(this.selectorsList().testField).eq(0).clear().type('Test Test', {delay: 150})
+            // cy.get(this.selectorsList().testField).eq(1).clear().type('The most common blood type in Brazil', {delay: 150})
+            cy.get(this.selectorsList().submitButton).eq(1).click({ multiple: true })
+            cy.get('.oxd-toast-close').click()
+            cy.get('body').should('contain', 'Successfully Saved')
+    }
+
+    SaveForm() {
             cy.get(this.selectorsList().submitButton).eq(1).click()
             cy.get('body').should('contain', 'Successfully Saved')
-            cy.get('.oxd-toast-close')
     }
+    
 }    
 
 export default MyInfoPage;
